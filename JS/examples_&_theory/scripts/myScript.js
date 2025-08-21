827778d8ff906f16dea5e745b88d30b2b2ec8f97
Jose Ruiz <joselrm91@gmail.com>
2025-08-21 13:36:20 +0200

    mil cosas

---
+++ JS/examples_&_theory/scripts/myScript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  document.getElementById("externalScript").innerHTML = "Párrafo cambiado.";
+}
+function myFunction02(){
+   document.getElementById("externalScript").innerHTML = "Podemos modificar elementos desde un link externo";
+}
+function myFunction03(){
+   window.alert("CUIDAO!!");;
+}
